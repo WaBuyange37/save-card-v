ad38a22f6b27bc39a18e0a4c90106782514f7122
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-05-04 15:08:43 +0200

    not well done but near the  way

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,21 @@ import Header from './components/headre/Header'
 import Typing from './components/Typing/Typing'
 
 function App() {
-  
+  const [people, setPeople] = useState([
+    { name: 'Kwizeera', age: '30', location: 'Gatsata', ocupation: 'Developer' },
+    { name: 'Manzi Karake', age: '28', location: 'Gatsibo', ocupation: 'Designer' }
+  ]);
 
   return (
     <>
       <Header></Header>
-      <Typing/>
+      <Typing card={people} setPeople={setPeople}/>
+
+      <div>
+        {people.map((kana,i)=>{
+          return <li key={i}>{kana.name}</li>
+        })}
+      </div>
     </>
   )
 }
--- src/components/Typing/Typing.tsx
+++ src/components/Typing/Typing.tsx
@@ -1,12 +1,37 @@
 
+import React,{ useState } from 'react';
 import styled from 'styled-components';
 const MySpan = styled.span`
   display: flex;
   justify-content: space-around;
 `
 
+interface Card{
+  name:string;
+  age: string;
+  location: string;
+  ocupation: string;
+}
+interface Props{
+  card: Card[];
+  setPeople: React.Dispatch<React.SetStateAction<Card[]>>;
+}
 
-const Typing = () => {
+const Typing = ({card, setPeople}:Props) => {
+  const [ikarita,setIkarita]= useState<{name:string;age:string;location:string;ocupation:string}[]>([]);
+
+  //for onchangeInput
+  const [injiza,setInjiza]= useState<{name:string;age:string;location:string;ocupation:string}>({
+    name: '',
+    age: '',
+    location: '',
+    ocupation: '',
+  });
+  
+  const mychange = (e: React.ChangeEvent<HTMLInputElement>)=>{
+    const {name, value}=e.target;
+    setInjiza(prev => ({...prev,[name]: value}));
+  }
   return (
     <div className='d-grid gap-3'>
       <form action="" style={{
@@ -14,14 +39,18 @@ const Typing = () => {
         flexDirection:'column'
       }}>
         <MySpan>
-        <label htmlFor="name">Name<input className='form-control ' id='name' type="text" /></label>
-        <label htmlFor="age">Age<input className='form-control ' id='age' type="text" /></label>
+        <label htmlFor="name">Name<input onChange={mychange} className='form-control ' name='name' id='name' type="text" /></label>
+        <label htmlFor="age">Age<input onChange={mychange} className='form-control ' name='age' id='age' type="text" /></label>
         </MySpan>
         <MySpan>
-        <label htmlFor="location">Location<input className='form-control ' id='location' type="text" /></label>
-        <label htmlFor="ocupation">Ocupations<input className='form-control ' id='ocupation' type="text" /></label>
+        <label htmlFor="location">Location<input onChange={mychange} className='form-control' name='location' id='location' type="text" /></label>
+        <label htmlFor="ocupation">Ocupations<input onChange={mychange} className='form-control' name='ocupation' id='ocupation' type="text" /></label>
         </MySpan>
-        <button className='btn btn-success'>See the card</button>
+        <button className='btn btn-success' onClick={(e)=>{
+          e.preventDefault();
+          setIkarita(prev =>[...prev, injiza])
+          
+        }}>See the card</button>
       </form>
     </div>
   )
